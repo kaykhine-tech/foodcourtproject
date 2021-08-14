0105c3216a617cae9f56ca7bbe920e9ee41613fb
kaykhine-tech <kaykhinekaykhine9@gmail.com>
2021-08-14 12:10:53 +0630

    Frontend Login Update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,14 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function authenticated($user){
+        $roles = Auth::user()->getRoleNames();
+        if($roles[0] == 'admin'){
+            return redirect()->route('dashboard.index');
+        }else{
+            return redirect($user->uri);
+        }
     }
 }
